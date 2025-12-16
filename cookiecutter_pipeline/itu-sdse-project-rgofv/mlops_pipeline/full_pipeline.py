1ae0fd773b0d20f1737bf45b61b10f1673790f13
poopiboi <158285218+poopiboi@users.noreply.github.com>
2025-12-17 00:06:48 +0100

    fix: Updated cli run-all & data fix
    run-all did not correctly call the local python environment on run.
    make_dataset had a variable naming error.

--- cookiecutter_pipeline/itu-sdse-project-rgofv/mlops_pipeline/full_pipeline.py
+++ cookiecutter_pipeline/itu-sdse-project-rgofv/mlops_pipeline/full_pipeline.py
@@ -1,13 +1,16 @@
 import subprocess
 from loguru import logger
+import sys
 
 
 def run_full_pipeline():
+    # We have to set the current .venv python
+    py_venv = sys.executable
     commands = [
-        ["python", "-m", "mlops_pipeline.cli.app", "data"],
-        ["python", "-m", "mlops_pipeline.cli.app", "build"],
-        ["python", "-m", "mlops_pipeline.cli.app", "prepare"],
-        ["python", "-m", "mlops_pipeline.cli.app", "train-model"],
+        [py_venv, "-m", "mlops_pipeline.cli.app", "data"],
+        [py_venv, "-m", "mlops_pipeline.cli.app", "build"],
+        [py_venv, "-m", "mlops_pipeline.cli.app", "prepare"],
+        [py_venv, "-m", "mlops_pipeline.cli.app", "train-model"],
     ]
 
     for cmd in commands:
